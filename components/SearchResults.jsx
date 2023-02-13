14e20077298087a3cf132bf9e5957d5f282d4084
Patryk Panek <patryyk221@gmail.com>
2023-02-13 17:17:01 +0100

    complete the result section and install html react parser

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Parser from 'html-react-parser';
 
 export default function SearchResults({ results }) {
   return (
@@ -7,6 +8,26 @@ export default function SearchResults({ results }) {
         About {results.searchInformation.formattedTotalResults} results (
         {results.searchInformation.formattedSearchTime} seconds)
       </p>
+      {results.items.map((item) => {
+        return (
+          <div key={item.link} className="max-w-xl mb-8">
+            <div className="group">
+              <a className="text-sm truncate" href={item.link}>
+                {item.formattedUrl}
+              </a>
+              <a
+                className="group-hover:underline decoration-blue-800"
+                href={item.link}
+              >
+                <h2 className="truncate text-xl font-medium text-blue-800">
+                  {item.title}
+                </h2>
+              </a>
+            </div>
+            <p>{Parser(item.htmlSnippet)}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 
 export default function search({ results }) {
   const router = useRouter();
-  console.log(results);
+
   return (
     <div>
       <Head>
